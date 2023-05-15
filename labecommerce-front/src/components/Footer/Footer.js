3babf67a6996a5d18ecdcfb289d77989b316147e
falcaomm <manu.m.falcao@gmail.com>
2023-05-15 19:36:57 -0300

    app e reajustes

--- labecommerce-front/src/components/Footer/Footer.js
+++ labecommerce-front/src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { DivStyle, FooterStyle, FooterTitle, Text, Test } from "./styles"
+import { DivStyle, FooterStyle, FooterTitle, Text} from "./styles"
 import { RiWhatsappFill } from 'react-icons/ri'
 import { GoMarkGithub } from 'react-icons/go'
 
@@ -8,12 +8,12 @@ function Footer() {
         <FooterStyle>
             <DivStyle>
                 <FooterTitle>Projeto Front-End React</FooterTitle>
-                <Text><a target="_blank" href="https://wa.me/5551992183683"><RiWhatsappFill />Contato</a>
+                <Text><a target="_blank" href="https://wa.me/5551992183683" rel="noreferrer"><RiWhatsappFill />Contato</a>
                 </Text>
             </DivStyle>
             <DivStyle>
                 <Text>
-                    Developed by <a target="_blank" href="https://github.com/falcaomm/projeto-frontendreact"><GoMarkGithub /> falcaomm</a>
+                    Developed by <a target="_blank" href="https://github.com/falcaomm/projeto-frontendreact" rel="noreferrer"><GoMarkGithub /> falcaomm</a>
                 </Text>
             </DivStyle>
 
